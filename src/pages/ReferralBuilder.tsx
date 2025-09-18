f7fe9fdc7354a802063eae7b52cacc2e118e9c23
atocayon27@gmail.com <atocayon27@gmail.com>
2025-09-19 02:52:47 +0800

    created Referral Builder page

---
+++ src/pages/ReferralBuilder.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ReferralBuilder: React.FC = () => {
+    return (
+        <div>
+            <h1>Referral Builder</h1>
+            <p>Welcome to the Referral Builder page!</p>
+        </div>
+    );
+};
+
+export default ReferralBuilder;
