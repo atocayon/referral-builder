154d83522d24c28f07e950d4cf708f14c08c1a57
atocayon27@gmail.com <atocayon27@gmail.com>
2025-09-19 18:25:13 +0800

    fetch referrals on page load

--- src/pages/ReferralBuilder.tsx
+++ src/pages/ReferralBuilder.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import ReferralForm from '../components/ReferralForm';
-import ReferralTable from '../components/ReferralTable';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useEffect } from "react";
+import ReferralForm from "../components/ReferralForm";
+import ReferralTable from "../components/ReferralTable";
+import { useDispatch, useSelector } from "react-redux";
+import type { AppDispatch, RootState } from "../store";
+import { fetchReferrals } from "../store/thunk/ReferralsThunk";
 
 const ReferralBuilder: React.FC = () => {
-    return (
-        <div className='referral-builder-page'>
-            <div className='referral-builder-page-form-container'>
-                <ReferralForm />
-            </div>
-            <div className='referral-builder-page-table-container'>
-                <ReferralTable referrals={[]} />
-            </div>
-            
-        </div>
-    );
+  const dispatch = useDispatch<AppDispatch>();
+    const { referrals } = useSelector((state: RootState) => state.referrals);
+    
+    useEffect(() => { 
+        dispatch(fetchReferrals());
+    }, [dispatch]);
+
+  const tableHeaders = ["Given Name", "Surname", "Email", "Phone", "Actions"];
+  return (
+    <div className="referral-builder-page">
+      <div className="referral-builder-page-form-container">
+        <ReferralForm />
+      </div>
+      <div className="referral-builder-page-table-container">
+        <ReferralTable referrals={referrals} thead={tableHeaders} />
+      </div>
+    </div>
+  );
 };
 
 export default ReferralBuilder;
